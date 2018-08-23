bcb4c1b680577a6a357f0be7c4162ea0efa1df90
DESKTOP-VT6V30U\irfan <dzankovicirfan@gmail.com>
2018-08-23 13:08:11 +0200

    JSON field in user for additional information Hunter integrated Clearbit integrated

--- account/models.py
+++ account/models.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import JSONField
 
 
 class User(AbstractUser):
 
     email = models.EmailField('Email address', blank=False, null=False)
+    additional_info = JSONField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'user'
--- account/serializers.py
+++ account/serializers.py
@@ -4,6 +4,12 @@ from __future__ import unicode_literals
 from rest_framework import serializers
 
 from .models import User
+from pyhunter import PyHunter
+import clearbit
+
+
+hunter = PyHunter('c60c4406617d0f1ad2c24526e5fa258c96a5a878')
+clearbit.key = 'sk_d4ff07e1dfde61281f450c0ae22936af'
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             'id', 'username', 'first_name', 'last_name', 'date_joined', 'email',
-            'password', 'is_active',
+            'password', 'is_active', 'additional_info'
         )
         extra_kwargs = {
             'password': {'write_only': True, 'required': True},
@@ -20,6 +26,19 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+        # komentarisano da ne bih pri testiranju trosio zahteve prema hunteru
+        # ogranicen je na 100 zahteva mesecno
+
+        email = validated_data['email']
+        # email_exist = hunter.email_verifier(email)
+
+        # if not email_exist:
+        #     raise serializers.ValidationError('Email does not exist')
+
+        person = clearbit.Enrichment.find(email=email, stream=True)
+
+        if person is not None:
+            validated_data['additional_info'] = person
 
         user = super(UserSerializer, self).create(validated_data)
         user.set_password(validated_data.get('password'))
