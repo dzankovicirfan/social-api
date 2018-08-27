e917d1e2c2f066f81f962002ef85093bb0a62e41
DESKTOP-VT6V30U\irfan <dzankovicirfan@gmail.com>
2018-08-27 12:22:48 +0200

    Test for account and entites app

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,129 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your tests here.
+from django.urls import reverse
+
+from rest_framework.test import APIClient, APITestCase
+
+from .models import User
+
+
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username='test',
+            email='test@email.com',
+            password='12345',
+            is_active=True
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('api:account:user-list')
+
+    def test_get_users_success(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+    def test_get_users_more_users_success(self):
+        self.user1 = User.objects.create(
+            username='test1',
+            email='test1@email.com',
+            password='123456',
+            is_active=True
+        )
+        self.user2 = User.objects.create(
+            username='test2',
+            email='test2@email.com',
+            password='123456',
+            is_active=True
+        )
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 3)
+
+    def test_post_user_success(self):
+        data = {
+            'username': 'testko',
+            'email': 'testko@email.com',
+            'password': '12345',
+            'is_active': True
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.objects.count(), 2)
+
+    def test_post_without_email_failed(self):
+        data = {
+            'username': 'testko',
+            'password': '12345',
+            'is_active': True
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_post_wrong_email_failed(self):
+        data = {
+            'username': 'testko',
+            'email': 'testkoemail.com',
+            'password': '12345',
+            'is_active': True
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_sign_up_alredy_exist_failed(self):
+        self.url = reverse('api:account:sign_up')
+
+        data = {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password': '12345',
+            'is_active': True
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_sign_up_success(self):
+        self.url = reverse('api:account:sign_up')
+
+        data = {
+            'username': 'test1',
+            'email': 'test1@email.com',
+            'password': '12345',
+            'is_active': True
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.objects.count(), 2)
+
+    def test_login_success(self):
+        self.url = reverse('api:account:login')
+        self.user1 = User.objects.create(
+            username='test1',
+            email='test1@email.com',
+            password='12345',
+            is_active=True
+        )
+        self.user1.set_password('12345')
+        self.user1.save()
+        response = self.client.post(self.url, {'username': 'test1', 'password': '12345'})
+
+        self.assertEqual(response.status_code, 200)
--- entities/tests.py
+++ entities/tests.py
@@ -1,3 +1,228 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your tests here.
+from django.urls import reverse
+
+from rest_framework.test import APIClient, APITestCase
+
+from .models import Post, Like
+from account.models import User
+
+
+class PostTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username='test',
+            email='test@email.com',
+            is_active=True
+        )
+        self.user.set_password('12345')
+        self.user.save()
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('api:entities:post-list')
+
+    def test_get_success(self):
+        self.post1 = Post.objects.create(
+            title='First post',
+            text='Some text',
+            user=self.user
+        )
+        self.post1 = Post.objects.create(
+            title='Second post',
+            text='Some text',
+            user=self.user
+        )
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+
+    def test_create_post_success(self):
+        data = {
+            'title': 'First post',
+            'text': 'Some text'
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['user'], self.user.id)
+
+    def test_update_post_success(self):
+        self.post1 = Post.objects.create(
+            title='First post',
+            text='Some text',
+            user=self.user
+        )
+        self.url = reverse('api:entities:post-detail', kwargs={'pk': self.post1.pk})
+
+        data = {
+            'title': 'Change title',
+            'text': 'New text'
+        }
+        response = self.client.put(self.url, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], 'Change title')
+        self.assertEqual(response.data['text'], 'New text')
+
+    def test_update_wrong_owner_failed(self):
+        self.user1 = User.objects.create(
+            username='test1',
+            email='test1@email.com',
+            is_active=True
+        )
+        self.post1 = Post.objects.create(
+            title='First post',
+            text='Some text',
+            user=self.user1
+        )
+
+        self.url = reverse('api:entities:post-detail', kwargs={'pk': self.post1.pk})
+
+        data = {
+            'title': 'Change title',
+            'text': 'New text'
+        }
+        response = self.client.put(self.url, data)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_success(self):
+        self.post1 = Post.objects.create(
+            title='First post',
+            text='Some text',
+            user=self.user
+        )
+        self.url = reverse('api:entities:post-detail', kwargs={'pk': self.post1.pk})
+
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, None)
+        self.assertEqual(Post.objects.count(), 0)
+
+    def test_delete_now_owner_failed(self):
+        self.user1 = User.objects.create(
+            username='test1',
+            email='test1@email.com',
+            is_active=True
+        )
+        self.post1 = Post.objects.create(
+            title='First post',
+            text='Some text',
+            user=self.user1
+        )
+
+        self.url = reverse('api:entities:post-detail', kwargs={'pk': self.post1.pk})
+
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Post.objects.count(), 1)
+
+
+class LikeTests(APITestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create(
+            username='test1',
+            email='test1@email.com',
+            is_active=True
+        )
+        self.user1.set_password('12345')
+        self.user1.save()
+
+        self.user2 = User.objects.create(
+            username='test2',
+            email='test2@mail.com',
+            is_active=True
+        )
+        self.user1.set_password('12345')
+        self.user1.save()
+
+        self.post1 = Post.objects.create(
+            title='First Post',
+            text='Post from user1',
+            user=self.user1
+        )
+
+        self.post2 = Post.objects.create(
+            title='Second Post',
+            text='Post from user2',
+            user=self.user2
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user1)
+        self.url = reverse('api:entities:like-list')
+
+    def test_get_like_success(self):
+        self.like1 = Like.objects.create(
+            post=self.post1,
+            user=self.user2
+        )
+        self.like2 = Like.objects.create(
+            post=self.post2,
+            user=self.user1
+        )
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+
+    def test_create_like(self):
+        data = {
+            'post': self.post2.pk
+        }
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['user'], self.user1.id)
+        self.assertEqual(Like.objects.count(), 1)
+
+    def test_like_your_own_post_failed(self):
+        data = {
+            'post': self.post1.pk
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Like.objects.count(), 0)
+
+    def test_delete_like_success(self):
+        self.like1 = Like.objects.create(
+            post=self.post1,
+            user=self.user2
+        )
+        self.like2 = Like.objects.create(
+            post=self.post2,
+            user=self.user1
+        )
+
+        self.url = reverse('api:entities:like-detail', kwargs={'pk': self.like2.pk})
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, None)
+        self.assertEqual(Like.objects.count(), 1)
+
+    def test_delete_like_failed(self):
+        self.like1 = Like.objects.create(
+            post=self.post1,
+            user=self.user2
+        )
+        self.like2 = Like.objects.create(
+            post=self.post2,
+            user=self.user1
+        )
+
+        self.url = reverse('api:entities:like-detail', kwargs={'pk': self.like1.pk})
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(Like.objects.count(), 2)
